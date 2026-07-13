309fd3ab482f12d19c313cd6812ffabce62792aa
Marcos Queiroz <marcosamfq123@gmail.com>
2026-07-12 21:32:21 -0300

    fix mini snippets C-j use

--- after/plugin/autocomplete.lua
+++ after/plugin/autocomplete.lua
@@ -1,3 +1,13 @@
-require('mini.completion').setup()
-require('mini.icons').setup()
-require('mini.snippets').setup()
+require("mini.completion").setup()
+require("mini.icons").setup()
+require("mini.snippets").setup(
+	{
+		mappings = {
+			expand = "<C-n>",
+
+			jump_next = "<C-l>",
+			jump_prev = "<C-h>",
+			stop = "<C-c>",
+		},
+	}
+)
